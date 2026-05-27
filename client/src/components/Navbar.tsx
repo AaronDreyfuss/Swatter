bbce563b24b2d2c7cac4abe301f618bf1496eaed
AaronDreyfuss <AaronDreyfuss@gmail.com>
2026-05-27 09:19:30 -0400

    Enhance Navbar with user email display

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -13,9 +13,14 @@ function Navbar() {
   };
 
   return (
+    // nav bar: flex row, space-between, padding, background, bottom border
     <nav>
       <Link to="/projects">Swatter</Link>
-      <button onClick={handleLogout}>Log out</button>
+      {/* right side: flex row, gap, align center */}
+      <div>
+        <span>{user.email}</span>
+        <button onClick={handleLogout}>Log out</button>
+      </div>
     </nav>
   );
 }
